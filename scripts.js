ae543cb02d5946652ac1bd006d4195b73c9538fb
Ashley <ash.r.diep@gmail.com>
2021-01-18 17:48:50 -0500

    Add etch-a-sketch functionality

--- scripts.js
+++ scripts.js
@@ -1,17 +1,20 @@
 function generateGrid(size)
 {
-    let container = document.querySelector(".container");
-
     for (let numRow = 1; numRow <= size; numRow++)
     {
         let row = document.createElement("div");
+        row.classList.add("row");
         for (let numCol = 1; numCol <= size; numCol++)
         {
             let square = document.createElement("div");
             square.classList.add("square");
 
-            square.style.width = `${gridSize / size}px`;
-            square.style.height = `${gridSize / size}px`;
+            square.addEventListener("mouseover", function() {
+                this.style.backgroundColor = "black";
+            })
+
+            square.style.width = `${pixelSize / size}px`;
+            square.style.height = `${pixelSize / size}px`;
             
             row.appendChild(square);
         }
@@ -20,6 +23,45 @@ function generateGrid(size)
     }
 }
 
-const gridSize = 650;
+function resetGrid()
+{
+    let squares = document.querySelectorAll(".square");
+    for (let i = 0; i < squares.length; i++)
+    {
+        squares[i].style.backgroundColor = "white";
+    }
+}
+
+function removeGrid()
+{
+    let rows = document.querySelectorAll(".row");
+    for (let i = 0; i < rows.length; i++)
+    {
+        rows[i].remove();
+    }
+}
+
+function getNewSize()
+{
+    gridSize = Number(prompt("How many squares per side? (0-99)"));
+    if (gridSize > 99)
+    {
+        alert("Maximum size exceeded.");
+    }
+    else
+    {
+        removeGrid();
+        generateGrid(gridSize);
+    }
+}
+
+const pixelSize = 650;
+let gridSize = 16;
 let container = document.querySelector(".container");
-generateGrid(16); // By default, 16 x 16 grid
+generateGrid(gridSize); 
+
+const reset = document.getElementById("reset");
+reset.addEventListener("click", resetGrid);
+
+const changeSize = document.getElementById("change-size");
+changeSize.addEventListener("click", getNewSize);
